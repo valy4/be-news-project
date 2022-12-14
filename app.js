fdda841b9b74e57b13922b3962ea01bee0d2acf2
Valentina <smeu.valentina33@gmail.com>
2022-12-14 13:20:38 +0000

    add getArticleById function and handleCustomPaths function

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const app = express();
-const { getTopics, getArticles } = require("./Controllers/controllers");
+const {
+  getTopics,
+  getArticles,
+  getArticlesById,
+} = require("./Controllers/controllers");
 const {
   handle404Paths,
+  handle400Paths,
+  handleCustomPaths,
   handle500Errors,
 } = require("./Controllers/controllers.errors");
 
 app.get("/api/topics", getTopics);
 app.get("/api/articles", getArticles);
+app.get("/api/articles/:article_id", getArticlesById);
 
 app.all("*", handle404Paths);
+app.use(handleCustomPaths);
+app.use(handle400Paths);
 
 app.use(handle500Errors);
 
